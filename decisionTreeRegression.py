1a4bd95ef0f5fe9d15068bc2ccceca343e8fe355
deepak <deepakrajesh567@gmail.com>
2023-08-17 08:31:38 +0530

    DecisionTreeRegression edited

--- decisionTreeRegression.py
+++ decisionTreeRegression.py
@@ -1,27 +1,22 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.tree import DecisionTreeRegressor,plot_tree
 from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("D:\S6\CloudLab\Iris.csv")
 
-# Select multiple features and reshape them
-X = df[["PetalLengthCm", "PetalWidthCm"]].values
-y = df["SepalLengthCm"].values
+x = df["PetalLengthCm"].values.reshape(-1,1)
+y = df["SepalLengthCm"].values.reshape(-1,1)
 
-# Reshape the features
-X = X.reshape(-1, 2)
-
-x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=99)
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 0.2,random_state=99)
 
 decision_tree = DecisionTreeRegressor()
-decision_tree.fit(x_train, y_train)
+
+decision_tree.fit(x_train,y_train)
+
 predicted = decision_tree.predict(x_test)
 
-plt.scatter(x_test[:, 0], y_test, color='red', label="Actual")
-plt.scatter(x_test[:, 0], predicted, color='blue', label='Predicted')
+plt.scatter(x_test,y_test,color = 'red',label = "Actual")
+plt.scatter(x_test,predicted,color='blue',label = 'predicted')
 plt.legend()
-plt.xlabel("Petal Length (cm)")
-plt.ylabel("Sepal Length (cm)")
-plt.title("Actual vs Predicted Sepal Length")
 plt.show()
